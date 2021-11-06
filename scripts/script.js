74314b45ad1af25796f6145e37220a2ab3a2dea7
YUHUI ZHOU <hughzhoutrt@gmail.com>
2021-11-06 11:33:32 -0400

    acieve the animation of curtain moving

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,13 @@
 const app = {};
 
 app.init = function() {
-    document.querySelector('#randomMovieButton').addEventListener('click', function() {
 
+    document.querySelector('#randomMovieButton').addEventListener('click', function() {
+        document.querySelector('#leftCurtain').style.left = '-30vw';
+        document.querySelector('#leftCurtain').style.transition = 'left 3s';
+        document.querySelector('#rightCurtain').style.right = '-30vw';
+        document.querySelector('#rightCurtain').style.transition = 'right 3s';
     });
 };
+
+app.init();
