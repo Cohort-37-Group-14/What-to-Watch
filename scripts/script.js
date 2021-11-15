3e2629fea16ebf18e2022b8614b123dbaf3cc738
YUHUI ZHOU <hughzhoutrt@gmail.com>
2021-11-15 11:35:09 -0500

    test public api

--- scripts/script.js
+++ scripts/script.js
@@ -3,14 +3,14 @@ const app = {};
 app.randomUrl = new URL('https://imdb-api.com/en/API/Top250Movies/');
 app.randomUrl.search = new URLSearchParams({
 // API keys for web establishments and tests:
-    apiKey: 'k_xpdojdru'
+    // apiKey: 'k_xpdojdru'
     // apiKey: 'k_jsfbzbhz'
     // apiKey: 'k_4eg4wtys'
     // apiKey: 'k_3349nupk'
     // apiKey:  'k_ya5sqa8y'
     // apiKey:  'k_0dsq0v17'
 // For netlify domain:
-    // apiKey: 'pk_9t16efqah2rok8tbg'
+    apiKey: 'pk_9t16efqah2rok8tbg'
 // For GitHub domain: 
     // apiKey: 'pk_07aylup2lutbytafc'
 });
@@ -158,7 +158,7 @@ app.specificPopup = function () {
             // app.specificApiKey =  'k_0dsq0v17';
             // app.specificApiKey = 'k_3349nupk';
         // For netlify domain:
-            // app.specificApiKey =  'pk_9t16efqah2rok8tbg'; 
+            app.specificApiKey =  'pk_9t16efqah2rok8tbg'; 
         // For GitHub domain:
             // app.specificApiKey =  'pk_07aylup2lutbytafc';  
             app.specificUrl = `https://imdb-api.com/en/API/Title/?apiKey=${app.specificApiKey}&id=${id}&options=FullCast%Posters%Trailer%Ratings`;
